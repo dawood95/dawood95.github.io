b9d8850d7622925ae39969aa8f52da382e55f5b4
Sheik Dawood <dawood95@gmail.com>
2020-12-08 04:28:14 -0500

    updated about

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,9 +2,8 @@
   <div class="content">
   <p class="is-size-4">About me</p>
   <p class="is-size-6">
-    I'm a PhD student at Purdue University working on deep neural networks w/ <a href="https://www.linkedin.com/in/eugenioculurciello">Eugenio Culurciello</a>. I'm passionate about using AI to help make the world a better place (atleast by a teeny tiny bit). I enjoy working in small teams and taking on big responsibilities.<br/>
-    I'm currently an ML Engineer Intern at Micron Technologies. Before this, I worked as a part-time Research Engineer at <a href="https://fwdnxt.com">FWDNXT</a> in conjunction with my PhD. I was also the Founding Engineer at <a href="https://www.perceiveinc.com">Perceive</a> before I decided to pursue my PhD.<br/>
-    Outside of work, I enjoy dancing (mostly competitive ballroom), playing MOBAs and cooking.
+    I'm a Machine Learning Researcher at Micron working on deep neural networks w/ <a href="https://www.linkedin.com/in/eugenioculurciello">Eugenio Culurciello</a>. I'm passionate about using AI to help make the world a better place (atleast by a teeny tiny bit). I enjoy working in small teams and tackling difficult problems..<br/>. Before this, I worked as a part-time Research Engineer at <a href="https://fwdnxt.com">FWDNXT</a> in conjunction with my PhD. I was also the Founding Engineer at <a href="https://www.perceiveinc.com">Perceive</a> before I decided to pursue my PhD.<br/>
+    Outside of work, I enjoy dancing (mostly competitive ballroom), playing DOTA and cooking.
   </p>
   </div>
 </template>
