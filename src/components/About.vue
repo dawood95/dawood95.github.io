df32982d88feff3d1fc2bab342cb4b867591f860
Sheik Dawood <dawood95@gmail.com>
2020-12-08 04:32:15 -0500

    --amend

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,7 +2,7 @@
   <div class="content">
   <p class="is-size-4">About me</p>
   <p class="is-size-6">
-    I'm a Machine Learning Researcher at Micron working on deep neural networks w/ <a href="https://www.linkedin.com/in/eugenioculurciello">Eugenio Culurciello</a>. I'm passionate about using AI to help make the world a better place (atleast by a teeny tiny bit). I enjoy working in small teams and tackling difficult problems..<br/>. Before this, I worked as a part-time Research Engineer at <a href="https://fwdnxt.com">FWDNXT</a> in conjunction with my PhD. I was also the Founding Engineer at <a href="https://www.perceiveinc.com">Perceive</a> before I decided to pursue my PhD.<br/>
+    I'm a Machine Learning Researcher at Micron working on deep neural networks w/ <a href="https://www.linkedin.com/in/eugenioculurciello">Eugenio Culurciello</a>. I'm passionate about using AI to help make the world a better place (atleast by a teeny tiny bit). I enjoy working in small teams and tackling difficult problems. Before this, I worked as a part-time Research Engineer at <a href="https://fwdnxt.com">FWDNXT</a> in conjunction with my PhD. I was also the Founding Engineer at <a href="https://www.perceiveinc.com">Perceive</a> before I decided to pursue my PhD.<br/>
     Outside of work, I enjoy dancing (mostly competitive ballroom), playing DOTA and cooking.
   </p>
   </div>
