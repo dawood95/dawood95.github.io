3061a9f59f345fefabe8f0e8f3e280a3b50350f4
Sheik Dawood <dawood95@gmail.com>
2024-01-12 12:14:55 -0800

    removing image optim

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
     output: "export",
     assetPrefix: isProd ? 'https://dawood95.github.io' : undefined,
+    images: {
+        unoptimized: true,
+    },
 }
 
 module.exports = nextConfig
